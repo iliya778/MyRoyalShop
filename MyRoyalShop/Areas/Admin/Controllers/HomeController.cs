1cc21e453ea379531ec2b7c7488d7fee6b8de000
Erfan <eshirkhanei261384@gmail.com>
2024-10-09 20:36:49 +0330

    Deleting the none for fun

--- MyRoyalShop/Areas/Admin/Controllers/HomeController.cs
+++ MyRoyalShop/Areas/Admin/Controllers/HomeController.cs
@@ -5,18 +5,10 @@ namespace MyRoyalShop.Areas.Admin.Controllers
     [Area("Admin")]
     public class HomeController : Controller
     {
-        // This is gay
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult actionResult()
-        {
-            return View();
-        }
-
-        // another comment to see the changes
 
     }
 }
